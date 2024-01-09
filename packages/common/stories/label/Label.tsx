852d1a8d57ea128d8f5c7e31d2ef1ac5bc9c81ec
kjin <pkjin.eng@gmail.com>
2024-01-09 13:36:45 +0900

    feat: add Label Component

---
+++ packages/common/stories/label/Label.tsx
@@ -0,0 +1,19 @@
+import AddCircle from "../assets/Icon";
+
+interface Props {
+  type: string;
+  children?: string;
+}
+
+export const Label = ({ type, children }: Props) => {
+  return (
+    <div className="flex items-center text-sm px-3 py-2 rounded-xl bg-sub-2 text-main">
+      {type === "WithIcon" && (
+        <AddCircle width="15" height="15" fill="fill-main" />
+      )}
+      <div className={type === "WithIcon" ? "pl-1 pb-0.5" : "pb-0.5"}>
+        {children}
+      </div>
+    </div>
+  );
+};
