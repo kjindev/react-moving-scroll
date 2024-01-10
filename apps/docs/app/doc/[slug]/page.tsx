41090040f88e11086ad1037e14d9aa77b6252eb7
kjin <pkjin.eng@gmail.com>
2024-01-10 15:32:21 +0900

    feat: update doc introduction content in apps/docs

--- apps/docs/app/doc/[slug]/page.tsx
+++ apps/docs/app/doc/[slug]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from "next/navigation";
 import { robotoBold } from "../../../util/font";
+import GettingStarted from "../../_components/doc/GettingStarted";
 
 export default function Page() {
   const { slug } = useParams();
@@ -22,39 +23,22 @@ export default function Page() {
     }
   };
 
+  const content = (slug: string) => {
+    if (slug === "getting-started") {
+      return <GettingStarted />;
+    } else if (slug === "quick-start") {
+      return;
+    }
+  };
   return (
     <div className="w-[1000px] pt-[60px] pl-[270px]">
       <div className="px-10 py-9">
-        <div>{category(slug as string)}</div>
+        <div className="text-sm">{category(slug as string)}</div>
         <div className={`${robotoBold.className} text-4xl`}>
           {title(slug as string)}
         </div>
-        <div className={`${robotoBold.className}`}>
-          ✅ What is SCROLL MOVING?
-        </div>
-        <div className={`${robotoBold.className}`}>✅ Installation</div>
-        <div className="text-sm my-3">
-          <span>Scroll Moving is available as a package on NPM.</span>
-          <Box>npm i scroll-moving</Box>
-          <span>Or if you are using yarn:</span>
-          <Box>yarn add scroll-moving</Box>
-          <span>Or if you are using pnpm:</span>
-          <Box>pnpm add scroll-moving</Box>
-        </div>
-        <div className={`${robotoBold.className}`}>✅ Getting Started!</div>
-        <div className="text-sm my-3">
-          If you finished to install, then you could import{" "}
-          <span className="font-bold">useScroll</span> in your project.
-        </div>
+        <div>{content(slug as string)}</div>
       </div>
-    </div>
-  );
-}
-
-function Box({ children }: { children: string }) {
-  return (
-    <div className="my-3 bg-zinc-800 text-zinc-100 text-sm px-5 py-4 rounded-xl">
-      {children}
     </div>
   );
 }
